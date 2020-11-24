8cad4ea7b6fcb11afa889b9f3bdf5ef173318f33
infosecirvin <il.tmri@pm.me>
2020-11-23 22:37:06 -0800

    main2.sh is working

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import subprocess
 import os
 #the web portion
 from flask import Flask
-app = Flask(_name_)
 
+app = Flask(__name__)
 @app.route("/")
 def rt():
-	hash = return "What is the hash?"
+	return "What is the hash?"
 
 if __name__ == "_main_":
 	app.run(host='0.0.0.0', port=3000)
--- main.sh
+++ main.sh
@@ -1,4 +1,50 @@
 #!/usr/bin/env bash
-echo "What is your LM hash?"
-read hash
-./rcracki_mt -h $hash ~/rt/lanman
+
+function lm {
+	echo "What is the LM hash?"
+	read lmhash
+	input=$(grep -i $lmhash results.txt)
+	search=$(echo "${input^^}")
+	match=$(echo $search | awk -F: '{print $1}')
+	if [ $match == $lmhash ]
+		then
+		answer=$(echo $search | awk -F: '{print $2}')
+		echo "The password is" $answer
+	else
+		./rcracki_mt -o results.txt -h $lmhash ~/git/rt/lanman/
+	fi
+}
+
+function sha {
+	echo "What is the MySQL or SHA1 hash?"
+	read shahash
+	input=$(grep -i $shahash results.txt)
+        search=$(echo "${input^^}")
+        match=$(echo $search | awk -F: '{print $1}')
+        if [ $match == $shahash ]
+                then
+                answer=$(echo $search | awk -F: '{print $2}')
+                echo "The password is" $answer
+        else
+                ./rcracki_mt -o results.txt -h $shahash ~/git/rt/mysqlsha1/
+        fi
+}
+
+function ntlm {
+	echo "ntlm"
+}
+
+echo "Rainbow Tables can be downloaded here: https://infocon.org/rainbow%20tables/"
+echo "What type of hash are you cracking?"
+echo "1) LM"
+echo "2) MySQL or SHA1"
+echo "3) NTLM"
+
+read choice
+if [[ $choice == 1 ]]; then
+	lm
+elif [[ $choice == 2 ]]; then
+	sha
+elif [[ $choice == 3 ]]; then
+	ntlm
+fi
